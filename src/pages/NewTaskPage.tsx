893c576f462d93e0f671fca7efafeab40405a47d
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2026-03-26 20:02:40 +0530

    pages setup

---
+++ src/pages/NewTaskPage.tsx
@@ -0,0 +1,10 @@
+function NewTaskPage() {
+  return (
+    <div>
+      <h1>New Task Page</h1>
+      <p>Form to create a new task will go here.</p>
+    </div>
+  );
+}
+
+export default NewTaskPage;
---
+++ src/pages/TaskListPage.tsx
@@ -0,0 +1,10 @@
+function TaskListPage() {
+  return (
+    <div>
+      <h1>Task List Page</h1>
+      <p>All tasks will be listed here.</p>
+    </div>
+  );
+}
+
+export default TaskListPage;
